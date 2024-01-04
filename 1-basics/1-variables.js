dc775d7731b918a0edc1bcfbb3ac2bbd70ca4447
Rohan Pardule <115710861+RohanPardule@users.noreply.github.com>
2024-01-04 11:48:07 +0000

    datatypes

---
+++ 1-basics/1-variables.js
@@ -0,0 +1,17 @@
+const accountId =144553
+let accountEmail="rohan@gmail.com"
+var password="1234"
+accountCity="jaipur"
+let accountState
+
+// accountId=2
+accountEmail="abc@gmail.com"
+password="777777"
+accountCity="mumbai"
+console.log(accountId);
+
+/* 
+prefer not to use var
+*/
+
+console.table([accountId,accountEmail,password,accountCity,accountState])
---
+++ 1-basics/2-datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all Js code as newer version
+
+ //symbol
+ //number
+ //bigint
+ //null = standalone object
+ //undefined= undefined type
+
+
+ //object
+ console.log(typeof null);
